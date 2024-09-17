534494cfe4913e6a6a70b8a37e4487c9219d4409
felippevianna <felippe.conduru@gmail.com>
2024-09-17 12:15:04 -0300

    Criacao do projeto

---
+++ src/main/java/com/tcc/rebone_3d/Rebone3dApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.rebone_3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rebone3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rebone3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/rebone_3d/Rebone3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.rebone_3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rebone3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
